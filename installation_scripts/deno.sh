c7fbabdd477e7f5e447206468796ccf170c0db69
Thodoris Sotiropoulos <theosotr@aueb.gr>
2025-09-16 01:08:26 +0300

    Update deno.sh

--- installation_scripts/deno.sh
+++ installation_scripts/deno.sh
@@ -2,12 +2,16 @@
 
 # Install Rust
 curl https://sh.rustup.rs -sSf | sh -s -- -y
+source ${HOME}/.cargo/env
 
-git clone https://github.com/grgalex/prv-deno deno
-git clone https://github.com/grgalex/prv-rusty_v8 rusty_v8
+sudo apt install cmake llvm clang pkg-config libgtk-3-dev
+
+git clone --recursive https://github.com/grgalex/prv-deno deno
+git clone --recursive https://github.com/grgalex/prv-rusty_v8 rusty_v8
 cd rusty_v8
 rm -rf v8
 git clone https://github.com/grgalex/prv-v8-deno v8
+cd ..
 git clone --branch 0.347.0 --single-branch https://github.com/denoland/deno_core
 cd deno
 V8_FROM_SOURCE=1 cargo --config .cargo/local-build.toml build
